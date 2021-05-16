18b4403011e4a06ab9a9e5be6635debfb29bc736
Tomasz Siłkowski <tmsilkowski@gmail.com>
2021-05-16 12:51:45 +0200

    Working, but not very efficiently

--- cards.py
+++ cards.py
@@ -7,15 +7,28 @@ def signum(a, b):
     elif b > a: return -1
     else:       return 0
 
+def listToString(inputList):
+    result = ''
+    for i in inputList:
+        result = result.join(str(i))
+    return result
+
 def popAndPass(inputList, toPop):
     result = inputList.copy()
     result.pop(toPop)
     return result;
 
+def addAndPass(inputList, toAdd):
+    result = inputList.copy()
+    result.append(toAdd)
+    return result
+
 def getGameResults(rewards, cards1, cards2):
     if len(rewards) == 1: return rewards[0] * signum(cards1[0], cards2[0])
-    elif gameResults.has_key([rewards, cards1, cards2]): return gameResults[[rewards, cards1, cards2]]
-    elif gameResults.has_key([rewards, cards2, cards1]): return gameResults[[rewards, cards1, cards2]] * (-1)
+    elif listToString([rewards, cards1, cards2]) in gameResults:
+        return gameResults[listToString([rewards, cards1, cards2])]
+    elif listToString([rewards, cards2, cards1]) in gameResults:
+        return gameResults[listToString([rewards, cards1, cards2])] * (-1)
     else:
         print("!ERROR! Did not find ", rewards, " ", cards1, " ", cards2, " state in result dictionary!")
         exit("0")
@@ -57,12 +70,60 @@ def solveNash(payoffMatrix):
     
     P = InteractiveLPProblem(A, b, c, variable_type=vt, constraint_type=ct)
     P = P.standard_form()
-    show(P)
+    #show(P)
     P.run_simplex_method()
-    print(P.optimal_solution())
-    print(P.objective_value(P.optimal_solution()))
-    return P.objective_value(P.optimal_solution())
+    #print(P.optimal_solution())
+    #print(P.objective_value(P.optimal_solution()))
+    return [P.objective_value(P.optimal_solution()), P.optimal_solution()]
 
-a = createPayoffMatrix([12, 13], 1, [2, 4], [1, 3])
-print(a)
-solveNash(a)
+#a = createPayoffMatrix([12, 13], 1, [2, 4], [1, 3])
+#print(a)
+#solveNash(a)
+
+def solveGame(rewards, cards1, cards2):
+    if listToString([rewards, cards1, cards2]) not in gameResults:
+        if len(rewards) > 1:
+            result = []
+            for r in range(len(rewards)):
+                payoffMatrix = createPayoffMatrix(rewards, r, cards1, cards2)
+                result.append(solveNash(payoffMatrix)[0])
+                gameResults[listToString([rewards, cards1, cards2])] = 1/len(rewards) * sum(result)
+
+def showMatrix(rewards, cards1, cards2):
+    result = []
+    for r in range(len(rewards)):
+        payoffMatrix = createPayoffMatrix(rewards, r, cards1, cards2)
+        result.append(solveNash(payoffMatrix)[1][:-2])
+    print(result)
+    return result
+
+def solveGamesRecursively(maxSize):
+    currSize = [] # [[rewards, cards1, cards2], [...], ...]
+    nextSize = [[[], [], []]] # [[rewards, cards1, cards2], [...], ...]
+
+    for size in range(maxSize):
+        currSize = nextSize
+        nextSize = []
+
+        for game in currSize:
+            if listToString(game) not in gameResults:
+                print(game)
+                for i in range(1, maxSize+1):
+                    if i not in game[0]:
+                        for j in range(1, maxSize+1):
+                            if j not in game[1]:
+                                for k in range(1, maxSize+1):
+                                    if k not in game[2]:
+                                        temp = [addAndPass(game[0], i),
+                                                addAndPass(game[1] , j),
+                                                addAndPass(game[2],  k)]
+                                        if temp not in nextSize: nextSize.append(temp)
+
+            if size >= 2:
+                solveGame(game[0], game[1], game[2])
+
+    final = nextSize[0]
+    showMatrix(final[0], final[1], final[2])
+
+
+solveGamesRecursively(5)
