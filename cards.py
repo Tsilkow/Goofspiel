e3948d6a1adf8b9e6e334f9a62c895ddd8459b9c
Tsilkow <69109124+Tsilkow@users.noreply.github.com>
2021-05-16 19:18:18 +0200

    Komentarze odnośnie celu fukncji

--- cards.py
+++ cards.py
@@ -4,27 +4,32 @@ import bisect
 
 gameResults = {}
 
+# Pomocnicza funkcja do określenia znaku liczby
 def signum(a, b):
     if   a > b: return 1
     elif b > a: return -1
     else:       return 0
 
+# Funkcja kodująca listę do haszowalnego stringa (to do: sprytniejsze kodowanie do inta?)
 def listToString(inputList):
     result = ''
     for i in inputList:
         result = result.join(str(i))
     return result
 
+# Pomocnicza funkcja kopiująca listę poza jednym elementem
 def popAndPass(inputList, toPop):
     result = inputList.copy()
     result.pop(toPop)
     return result;
 
+# Pomocnicza funkcja kopiująca listę z dodatkowym (i posortowanym) elementem
 def addAndPass(inputList, toAdd):
     result = inputList.copy()
     bisect.insort(result, toAdd)
     return result
 
+# Funkcja poszukująca i zwracająca rezultat danej konfiguracji; jeśli taki nie jest policzony, zwracany jest fałsz
 def getGameResults(rewards, cards1, cards2):
     if len(rewards) == 1: return rewards[0] * signum(cards1[0], cards2[0])
     elif cards1 == cards2:
@@ -34,14 +39,9 @@ def getGameResults(rewards, cards1, cards2):
     elif listToString([rewards, cards2, cards1]) in gameResults:
         return gameResults[listToString([rewards, cards2, cards1])] * (-1)
     else:
-        print("!ERROR! Did not find ", rewards, " ", cards1, " ", cards2, " state in result dictionary!")
-        exit("0")
-        
-#def getGameResults(rewards, cards1, cards2):
-#    a = getGameResultsP(rewards, cards1, cards2)
-#    print(rewards, cards1, cards2, a)
-#    return a
+        return False
 
+# Funkcja tworząca matrycę wypłat dla danej konfiguracji i wybranej nagrody
 def createPayoffMatrix(rewards, rewardIndex, cards1, cards2):
     result = []
     for c1 in range(len(cards1)):
@@ -54,8 +54,8 @@ def createPayoffMatrix(rewards, rewardIndex, cards1, cards2):
     return result
     
 
-def solveNash(payoffMatrix):
-    
+# Funkcja odnajdująca równowagę Nasha
+def solveNash(payoffMatrix):    
     mb = [0 for i in range(len(payoffMatrix))]
     mb.append(1)
     mc = [0 for i in range(len(payoffMatrix[0]))]
@@ -76,14 +76,9 @@ def solveNash(payoffMatrix):
     P = P.standard_form()
     #show(P)
     P.run_simplex_method()
-    #print(P.optimal_solution())
-    #print(P.objective_value(P.optimal_solution()))
-    return [P.objective_value(P.optimal_solution()), P.optimal_solution()]
+    return [P.objective_value(P.optimal_solution()), P.optimal_solution()[:-2]]
 
-#a = createPayoffMatrix([12, 13], 1, [2, 4], [1, 3])
-#print(a)
-#solveNash(a)
-
+# Funkcja rozwiązująca konkretną konfigurację gry - pod warunkiem że konfiguracje mniejszych długości są rozwiązane
 def solveGame(rewards, cards1, cards2):
     if listToString([rewards, cards1, cards2]) not in gameResults:
         if len(rewards) > 1:
@@ -93,11 +88,12 @@ def solveGame(rewards, cards1, cards2):
                 result.append(solveNash(payoffMatrix)[0])
                 gameResults[listToString([rewards, cards1, cards2])] = 1/len(rewards) * sum(result)
 
+# Funkcja tworząca finalną matrycę
 def showMatrix(rewards, cards1, cards2):
     result = []
     for r in range(len(rewards)):
         payoffMatrix = createPayoffMatrix(rewards, r, cards1, cards2)
-        result.append(solveNash(payoffMatrix)[1][:-2])
+        result.append(solveNash(payoffMatrix)[1])
 
     decimal_result = []
     for r in range(len(result)):
@@ -108,6 +104,7 @@ def showMatrix(rewards, cards1, cards2):
     print(decimal_result)
     return decimal_result
 
+# Funkcja wywołująca rozwiązywanie pojedyńczych konfiguracji w odpowiedniej kolejności
 def solveGamesRecursively(maxSize):
     currSize = [] # [[rewards, cards1, cards2], [...], ...]
     nextSize = [[[], [], []]] # [[rewards, cards1, cards2], [...], ...]
@@ -127,18 +124,15 @@ def solveGamesRecursively(maxSize):
                                 for k in range(1, maxSize+1):
                                     if k not in game[2]:
                                         temp = [addAndPass(game[0], i),
-                                                addAndPass(game[1] , j),
-                                                addAndPass(game[2],  k)]
-                                        if temp not in nextSize \
-                                           and [temp[0], temp[2], temp[1]] not in nextSize \
-                                           and temp[1] != temp[2]:
-                                            nextSize.append(temp)
+                                                addAndPass(game[1], j),
+                                                addAndPass(game[2], k)]
+                                        if !getGameResults(temp): nextSize.append(temp)
 
             if size >= 2:
                 solveGame(game[0], game[1], game[2])
 
-    final = [[i+1 for i in range(maxSize)], [i+1 for i in range(maxSize)], [i+1 for i in range(maxSize)]]
-    showMatrix(final[0], final[1], final[2])
+    final = [i+1 for i in range(maxSize)]
+    showMatrix(final, final, final)
 
 maxSize = 3
 if len(sys.argv) > 1: maxSize = int(sys.argv[1])
